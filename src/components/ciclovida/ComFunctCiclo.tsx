ad05dbcb762178ecc6e31c011a6cf1631957ffbd
Marcos Lopez <marcos.lopez@scrm.lidl>
2023-02-17 09:45:37 +0100

    tema 4 console logs

--- src/components/ciclovida/ComFunctCiclo.tsx
+++ src/components/ciclovida/ComFunctCiclo.tsx
@@ -16,7 +16,18 @@ const ComFunctCiclo = () => {
     <div>
       <button
         onClick={() => {
+          console.time("mi contador");
+
+          const alumnos = [
+            { nombre: "marc", edad: 12 },
+            { nombre: "marc os", edad: 122 },
+            { nombre: "marcos", edad: 132 },
+          ];
+          console.table(alumnos);
+          console.log("alumnos", alumnos);
+          console.log({ alumnos });
           setMiVariable((val) => !val);
+          console.timeEnd("mi contador");
         }}
       >
         actualiza
