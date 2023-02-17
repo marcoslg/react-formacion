7c587bb87f838c7aebd98f2a076d463f38ae2931
Marcos Lopez <marcos.lopez@scrm.lidl>
2023-02-17 14:13:26 +0100

    tareas

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 // import Contact from "./views/Contact";
 // import Home from "./views/Home";
 // import Login from "./views/Login";
-import { Admin, Contact, Home, Login } from "views";
+import { Admin, Contact, Home, Login, Tareas } from "views";
 
 function App() {
   const routes = {
@@ -14,7 +14,7 @@ function App() {
     login: <Login />,
   };
   // let currtenRoute: "home" | "contact" | "admin" | "login" = "contact";
-  let currtenRoute: string = "login";
+  let currtenRoute: string = "tareas";
   // return routes[currtenRoute];
   // if (currtenRoute === "home") return <Home />;
   // if (currtenRoute === "contact") return <Contact />;
@@ -27,6 +27,7 @@ function App() {
       {currtenRoute === "contact" && <Contact />}
       {currtenRoute === "admin" && <Admin />}
       {currtenRoute === "login" && <Login />}
+      {currtenRoute === "tareas" && <Tareas />}
       {/* {currtenRoute === "home" ? "hola estoy en home" : "no estoy en home"} */}
     </div>
   ) : (
