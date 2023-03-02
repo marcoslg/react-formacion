7cec8f6f1906de4ff6af5487f45eca187ce1024c
Marcos Lopez <marcos.lopez@scrm.lidl>
2023-03-02 13:32:59 +0100

    environments

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,16 @@ const Lista = lazy(() => import("views/Lista"));
 function App() {
   const [route, setRoute] = useState("lista");
   const cambia = () => setRoute("contador");
+  const entorno = process.env.NODE_ENV;
   return (
     <div>
       <Suspense fallback={<div>Cargando</div>}>
+        <h1>{process.env.REACT_APP_NAME}</h1>
         {route == "contador" && <Contador />}
         {route == "lista" && <Lista />}
         <button onClick={cambia}>cambia</button>
+
+        <div>entorno: {entorno}</div>
       </Suspense>
     </div>
   );
